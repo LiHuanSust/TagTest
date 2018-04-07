fa5c123b7f347f5f6389d6ae1709ba69ab45e9af
LiHuanSust <18392953499@163.com>
2018-04-07 14:37:36 +0800

    2018.4.7数据库添加之前修正

--- app/src/main/java/com/example/tagtest/FragmentCost.java
+++ app/src/main/java/com/example/tagtest/FragmentCost.java
@@ -5,7 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,6 +20,10 @@ import java.util.ArrayList;
 
 public class FragmentCost extends Fragment implements View.OnClickListener{
     private TextView cost_type;
+    private EditText cost_money;
+    private EditText cost_remarks;
+    protected Spinner spinner_card_select;//银行卡下拉框
+    protected LinearLayout bank_card_layout;
     private Button button_1;
     private Button button_2;
     private Button button_3;
@@ -28,7 +36,13 @@ public class FragmentCost extends Fragment implements View.OnClickListener{
     private Button button_10;
     private Button button_11;
     private Button button_12;
+    private Button saveButton; //保存按钮
+    private Button clearButton;//清空按钮
+    protected Spinner spinner_cost_type; //消费类型下拉框
     private ArrayList<Button> list_button=null;
+    public String cost_type_value=null;
+    private String bank_select_value=null;
+    private String costMoney=null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup contain, Bundle bundle)
@@ -41,6 +55,7 @@ public class FragmentCost extends Fragment implements View.OnClickListener{
     {
         super.onActivityCreated(savedInstanceState);
         initialise();
+        costTypeSelect();
 
 
 
@@ -60,7 +75,14 @@ public class FragmentCost extends Fragment implements View.OnClickListener{
         button_10=getActivity().findViewById(R.id.button10);
         button_11=getActivity().findViewById(R.id.button11);
         button_12=getActivity().findViewById(R.id.button12);
-        cost_type=getActivity().findViewById(R.id.cost_type);
+        saveButton=getActivity().findViewById(R.id.cost_add_save);
+        clearButton=getActivity().findViewById(R.id.cost_add_cancel);
+        cost_type=getActivity().findViewById(R.id.cost_type); //消费类型显示,TextView
+        cost_remarks=getActivity().findViewById(R.id.cost_remarks); //备注，EditText
+        cost_money=getActivity().findViewById(R.id.cost_money); //消费金额，TextView
+        spinner_cost_type=(Spinner) getActivity().findViewById(R.id.spinner_cost_type);//消费类型选择，Spinner
+        bank_card_layout=(LinearLayout)getActivity().findViewById(R.id.bank_card_select);//银行卡选择，Spinner
+
         list_button.add(button_1);
         list_button.add(button_2);
         list_button.add(button_3);
@@ -73,11 +95,51 @@ public class FragmentCost extends Fragment implements View.OnClickListener{
         list_button.add(button_10);
         list_button.add(button_11);
         list_button.add(button_12);
+        list_button.add(saveButton);
+        list_button.add(clearButton);
         for(Button button:list_button)
         {
             button.setOnClickListener(this);
         }
+    }
+    public void costTypeSelect()
+    {
+        spinner_cost_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                bank_card_layout.setVisibility(View.GONE);
+                cost_type_value=adapterView.getItemAtPosition(i).toString();
+                if(cost_type_value!=null && cost_type_value.equals("银行卡"))
+                {
+                   bankCardSelected();
+                   // Toast.makeText(getActivity(),(spinner_card_select.getVisibility())+" ",Toast.LENGTH_SHORT).show();
 
+
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+    public void bankCardSelected()   //银行卡选择
+    {
+        bank_card_layout=(LinearLayout)getActivity().findViewById(R.id.bank_card_select);
+        bank_card_layout.setVisibility(View.VISIBLE);
+        spinner_card_select=(Spinner)getActivity().findViewById(R.id.spinner_bank_select);
+        spinner_card_select.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                bank_select_value=adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
     @Override
@@ -145,6 +207,18 @@ public class FragmentCost extends Fragment implements View.OnClickListener{
                 cost_type.setText(button_12.getText().toString());
                 button_12.setSelected(true);
                 break;
+            case R.id.cost_add_cancel:
+                cost_type.setText(button_1.getText().toString());
+                setNotSelected();
+                button_1.setSelected(true);
+                cost_money.setText("");
+                cost_remarks.setText("");
+                spinner_cost_type.setSelection(0);
+                bank_card_layout.setVisibility(View.GONE);
+                break;
+            case R.id.cost_add_save:
+
+
                 default:
                     break;
 
--- app/src/main/java/com/example/tagtest/MyDate.java
+++ app/src/main/java/com/example/tagtest/MyDate.java
@@ -4,20 +4,37 @@ package com.example.tagtest;
  */
 
 public class MyDate {
-    private String type;
-    private String data;
-    private String money;
+    private String user; //用户id
+    private boolean type; //消费或收入的标识 当type为true表示支出，当为false表示收入
+    private String typeSelect; //消费或收入的具体类别
+    private String solution; //付款方式或收入方式
+    private String data; //日期
+    private String money;//具体金额
+    private String bank; //银行选择
+    private String remarks;
     public MyDate()
     {}
-    public MyDate(String type,String data,String money)
+    public MyDate(String typeSelect,String data,String money) //此接口是为首页展示提供数据
     {
-        this.type=type;
+        this.typeSelect=typeSelect;
         this.data=data;
         this.money=money;
     }
+    public MyDate(String user,boolean type,String typeSelect,String solution,String data,String money,String bank,String remarks) //此接口是数据库存储调用
+    {
+        this.user=user;
+        this.type=type;
+        this.typeSelect=typeSelect;
+        this.solution=solution;
+        this.data=data;
+        this.money=money;
+        this.bank=bank;
+        this.remarks=remarks;
+
+    }
     public String getType()
     {
-        return type;
+        return typeSelect;
     }
     public String getData()
     {
@@ -27,9 +44,9 @@ public class MyDate {
     {
         return money;
     }
-    public void setType(String type)
+    public void setSelectType(String type)
     {
-        this.type=type;
+        this.typeSelect=type;
     }
     public void setData(String data)
     {
